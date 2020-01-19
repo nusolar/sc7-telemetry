396a9ff0f537060f1e985275f37908bc854c2ed7
Charlie Zheng <chzheng@protonmail.com>
2020-01-19 15:57:23 -0600

    Massive CSV table format

---
+++ logtable.py
@@ -0,0 +1,37 @@
+"""Creates a massive CSV table of packet values"""
+
+from receiver import Receiver
+from tempfile import TemporaryFile
+
+with TemporaryFile(mode='w+t') as tempfile,\
+    open('log.csv', mode='w') as logfile:
+    receiver = Receiver()
+    headers = []
+    for packet in receiver.get_packets_from_file('examples/live_capture.txt'):
+        #Skip invalid packets
+        if 'Name' not in packet.keys():
+            continue
+        #Include Cell ID in name
+        if 'Cell ID' in packet.keys():
+            packet['Name'] = packet['Name'] + ' ' + str(packet['Cell ID'])
+            del packet['Cell ID']
+        #Append name to all keys
+        name = packet['Name'] + ' '
+        del packet['Name']
+        for key in list(packet):
+            packet[name + key] = packet[key]
+            #Add nonexistent headers
+            if name + key not in headers:
+                headers.append(name + key)
+            del packet[key]
+        #Write to CSV
+        result = []
+        for header in headers:
+            if header in packet.keys():
+                result.append(str(packet[header]))
+            else:
+                result.append('')
+        tempfile.write(','.join(result) + '\n')
+    logfile.write(','.join(headers) + '\n')
+    tempfile.seek(0)
+    logfile.write(tempfile.read())
